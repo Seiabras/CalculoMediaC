e87616f887563d019720fee06bd0703937404783
MatheusVega <126572587+Seiabras@users.noreply.github.com>
2023-08-29 15:42:10 -0300

    Finalização.
    Acabei o programa, com todas as especificações.

--- propriedade_mat.c
+++ propriedade_mat.c
@@ -4,17 +4,39 @@ int N, c, d, u;
 
 int main(){
     printf("Me dê um número inteiro de três digitos: ");
-    scanf("%d", &N);
     
-    //não deixar dar um número que não tenha 3 dígitos
-    if(N <= 99 || N >=1000 || N == float){
-        printf("Lê as instuções de novo"); 
+    //não deixar números que não sejam inteiros
+    if(scanf("%d", &N) != 1){
+        printf("Lê as instruções de novo.\n");
+        printf("Só são aceitos numeros inteiros.");
         return 0;
     }
     
-    //u = N%10
-    printf("%d", N);
     
+    //não deixar dar um número que não tenha 3 dígitos
+    if(N < 100 || N >=1000){
+        printf("Lê as instruções de novo.\n"); 
+        printf("Só são aceitos números de 3 dígitos.");
+        return 0;
+    }
+    
+    c = N/100;
+    printf("\nA centena desse número é: %d\n", c);
+    d = (N/10)%10;
+    printf("A dezena desse número é: %d\n", d);
+    u = N%10;
+    printf("A unidade desse número é: %d\n\n", u);
+    
+    if(c*c*c + d*d*d + u*u*u == N){
+    printf("Segundo a proposta, %d = %d^3 + %d^3 + %d^3\n", N, c, d, u);
+    printf("Dessa forma, %d = %d + %d + %d\n", N, c*c*c, d*d*d, u*u*u);
+    printf("Satisfazendo a condição de %d ser igual a %d.\n", N, c*c*c + d*d*d + u*u*u);
+    }else{
+    printf("Segundo a proposta, %d = %d^3 + %d^3 + %d^3\n", N, c, d, u);
+    printf("Dessa forma, %d = %d + %d + %d\n", N, c*c*c, d*d*d, u*u*u);
+    printf("Infelizmente esse número não satisfaz a condição: %d eh diferente de %d", N, c*c*c + d*d*d + u*u*u);
+    
+    }
 
     return 0;
 }
