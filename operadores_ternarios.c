64543820a7c00d42c8c0067134a2b00628544213
MatheusVega <126572587+Seiabras@users.noreply.github.com>
2023-09-02 17:40:14 -0300

    Update operadores_ternarios.c

--- operadores_ternarios.c
+++ operadores_ternarios.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
+//int main(void) {
+//
+//   int x;
+//
+//   printf("Digite um inteiro: ");
+//    scanf("%d", &x);
+//    x = (x % 2 == 1 ) ? 1 : -1;
+//
+//    if(x == 1){
+//    printf("O valor dado eh ímpar.");
+//    return 0;
+//    }else
+//    printf("O valor dado eh par.");
+//}
+
+//int main(void) {
+//
+//   float x;
+//
+//    printf("Digite um racional: ");
+//    scanf("%f", &x);
+//    
+//    if(x > 0){
+//    printf("O valor dado eh positivo.");
+//    return 1;
+//   }else
+//    printf("O valor dado eh negativo ou 0.");
+//    return 0;
+//}
+
+//int main(void) {
+//
+//    float x, y, z, m;
+//
+//    printf("Digite 3 racionais separados por espaço: ");
+//    scanf("%f %f %f", &x, &y, &z);
+//    
+//  m = (x > y) ? ((x > z) ? x : z) : (y > z ? y : z);
+//    printf("%f", m);
+//  return 0;
+//}
+
+//int main(void) {
+//
+//    float x, y, z, m, n;
+//
+//    printf("Digite 3 racionais separados por espaço: ");
+//    scanf("%f %f %f", &x, &y, &z);
+//    
+//    m = x > y ? (x > z ? x : z) : y > z ? y : z;
+//    n = (x == m) ? (y > z ? y : z) : (y == m) ? (x > z ? x : z) : (x > y ? x : y);
+//    
+//    printf("%f", (m + n) / 2.0);
+//    return 0;
+//}
+
 int main(void) {
 
-    int x;
+    char x, resultado;
 
-    printf("Digite um inteiro: ");
-    scanf("%d", &x);
-    x = (x % 2 == 1 ) ? 1 : -1;
-
-    if(x == 1){
-    printf("O valor dado eh ímpar\n");
+    printf("Digite uma letra: ");
+    scanf("%c", &x);
+    
+    resultado = (x >= 97 && x <= 122) ? 'm' : (x >= 65 && x <= 90) ? 'M' : '?';
+    
+    printf("%c", resultado);
+    
     return 0;
-    }else
-    printf("O valor dado eh par\n");
 }
